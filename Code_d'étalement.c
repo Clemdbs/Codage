958b39c353523e4cdd366d904e8d2e4742d9edea
Dubois Clement <s174109@ic2s120-05.univ-lemans.fr>
2019-11-20 14:33:16 +0100

    debut

--- Code_d'étalement.c
+++ Code_d'étalement.c
@@ -1,6 +1,28 @@
 //Ecriture de l’ensemble des fonctions composants la mise en oeuvre du codage deHadamard. Ajout de la technique des séquences d’étalement.
+//matrice hadamard : x^n-1 | x^n-1
+//                   x^n-1 |-x^n-1
+//x[0]=1
+//nbUilisateur % 2 tant que c'est possible pour le nb de matrices à faire
+//Recopier les matrices d'avant pour éviter les répétitions de calculs
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+void hadamard(int longueur){
+    int H[][]//Matrice de hadamard
+    int i, j;
+    int tempH[][]//Matrice de save de celle d'hadamard
+}
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, const char* argv[]){
     
