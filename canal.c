d0c5cf73917b161b7b08600fa58cb4ad82bd4757
Farizon Noemie <s174383@ic2s120-06.univ-lemans.fr>
2019-11-27 10:16:13 +0100

    canal+TP1B

--- canal.c
+++ canal.c
@@ -6,14 +6,28 @@
 #include <sys/msg.h>
 #include <signal.h>
 
-void hand(int sig){
-     continu = 0;
-}
-
 int main(){
     int id;
     demande_t demande;
     reponse_t reponse;
 
-    demande.
+    demande.type = TYPE;
+    demande.corps.pid_client = getpid();
+
+    if((id = msgget(CLE, 0666))==-1){
+        perror("pb recherche file");
+        exit(-3);
+    }
+
+    do{
+        if(msgsnd(id, &demande, sizeof(struct corps), 0) == -1){
+            perror("pb msgsnd");
+            exit(-4);
+        }
+        if(msgrcv(id, &reponse, sizeof(???????), getpid(), 0)==-1){
+            perror("pb msgrcv");
+            exit(-5);
+        }
+    }while();
+
 }
--- canal.h
+++ canal.h
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 
+#define CLE 10
+#define TYPE 1
+
+typedef struct corps{
+    pid_t pid_client;
+    char msg[256];
+}corps_t;
 
 typedef struct demande{
-
+    long type;
+    corps_t corps;
 }demande_t;
 
 typedef struct reponse{
-
+    long type;
+    char msg[256];
 }reponse_t;
