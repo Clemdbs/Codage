1d05417fc5fa4b33ba3b1c0ed7c8a36447515cda
Noémie Farizon <farizonnoemie@MacBook-Pro-Noemie.local>
2019-12-06 11:29:28 +0100

    correction

--- TP1B/arithmetique.c
+++ TP1B/arithmetique.c
@@ -16,14 +16,14 @@ float alpha[27] = {0}; //initialiser toutes les cases du tableau a 0
 
 // compter le nombre d'occurence de chaque lettres du message
 for(i = 0; ca != '\0', i++){
-    ci = atoi(getchar(ca));// ne marche pas 
+    ci = atoi(message[i]);
     position = ci - 'A';
     alpha[position] += 1;//incrémenter la case correspondant au caractere courant
 }
 // transforme frequence (fraction)
 for(j = 0, k = 0; k < 27; k++){
-    if(alpha[j] != 0){
-        alpha[j] /= tailleMessage;
+    if(alpha[k] != 0){
+        alpha[k] /= tailleMessage;
         j++;//devient le nombre de caracteres different utilisé
     } 
 }
@@ -48,10 +48,10 @@ for(k = 0; k < j; k++){
 long float code[2] = [0, 0];
 int dif; // ecart entre les deux bornes de l'intervalle
 // codage
-do{
-    ci = atoi(getchar(ca));// ne marche pas 
+for(i = 0; ca != '\0', i++){
+    ci = atoi(message[i]);
     position = ci - 'A';
     dif = code[0] - code[1];
     code[0] = code[1] + ( dif * alpha[position - 1]);
     codeCourant[1] = code[1] + ( dif * alpha[position]);
-}while(ca != '\0');
+}
